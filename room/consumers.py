013aeab7584f865941d723c1eb35bb37c12dec63
Sahith Akula <akulasahith@example.com>
2025-07-15 19:20:44 +0530

    Initial commit

---
+++ room/consumers.py
@@ -0,0 +1,66 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+from asgiref.sync import sync_to_async
+from django.contrib.auth.models import User
+from .models import Message, Room, Open_room
+import logging
+
+logger = logging.getLogger(__name__)
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        message = data['message']
+        username = data['username']
+        room = data['room']
+
+        await self.save_message(username, room, message)
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'username': username,
+                'room': room,
+            }
+        )
+
+    async def chat_message(self, event):
+        message = event['message']
+        username = event['username']
+        room = event['room']
+
+        await self.send(text_data=json.dumps({
+            'message': message,
+            'username': username,
+            'room': room,
+        }))
+
+    @sync_to_async
+    def save_message(self, username, room, message):
+        try:
+            user = User.objects.get(username=username)
+            room_obj = Room.objects.get(name=room)
+            Message.objects.create(user=user, room=room_obj, content=message)
+        except Room.DoesNotExist:
+            logger.error(f"Room with name '{room}' does not exist.")
+        except User.DoesNotExist:
+            logger.error(f"User with username '{username}' does not exist.")
